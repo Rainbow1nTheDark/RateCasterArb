1da7ab3fda8d4a2be3797c34bdc973ca369ecc31
Rainbow1nTheDark <hai1rakegg3@gmail.com>
2024-09-26 10:55:25 -0400

    Added Logo

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     description,
   },
   icons: {
-    icon: [{ url: "/optimist_star.webp", sizes: "32x32", type: "image/png" }],
+    icon: [{ url: "/logo.jpg", sizes: "32x32", type: "image/png" }],
   },
 };
 
